512974bfcd34c3a99d2e0f1050994775206e3b5b
frdytheme <rbfridayy@gmail.com>
2023-11-13 20:25:20 +0900

    Title: 코로나 메뉴얼, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231113/코로나 메뉴얼/covid-manual.py
+++ 231113/코로나 메뉴얼/covid-manual.py
@@ -2,8 +2,8 @@ A, B, C = input().split(), input().split(), input().split()
 a, b, c = A[0], B[0], C[0]
 ac, bc, cc = int(A[1]), int(B[1]), int(C[1])
 
-if ac + bc >= 74 or ac + cc >= 74 or bc + cc >= 74:
-    if a+b == "YY" or a+c == "YY" or b + c == "YY":
+if a+b == "YY" or a+c == "YY" or b + c == "YY":
+    if (a >= 36 and b >= 36) or (a >= 36 and c >= 36) or (b >= 36 and c >= 36):
         print("E")
 else:
     print("N")
