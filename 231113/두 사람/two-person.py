ae18a4a55fbdcb83d3115b0a6b7ce88b36d42172
frdytheme <rbfridayy@gmail.com>
2023-11-13 19:54:31 +0900

    Title: 두 사람, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 231113/두 사람/two-person.py
+++ 231113/두 사람/two-person.py
@@ -4,4 +4,4 @@ B = input().split()
 a_age, a = int(A[0]), A[1]
 b_age, b = int(B[0]), B[1]
 
-print(int((a_age >= 19 and a == "M") or (b_age >= 19 or b == "M")))
+print(int((a_age >= 19 and a == "M") or (b_age >= 19 and b == "M")))
