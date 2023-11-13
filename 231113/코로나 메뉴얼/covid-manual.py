38e4d166c36d1e118b2b07b248ceeedebcc34848
frdytheme <rbfridayy@gmail.com>
2023-11-13 20:26:09 +0900

    Title: 코로나 메뉴얼, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231113/코로나 메뉴얼/covid-manual.py
+++ 231113/코로나 메뉴얼/covid-manual.py
@@ -3,7 +3,7 @@ a, b, c = A[0], B[0], C[0]
 ac, bc, cc = int(A[1]), int(B[1]), int(C[1])
 
 if a+b == "YY" or a+c == "YY" or b + c == "YY":
-    if (ac >= 36 and bc >= 36) or (ac >= 36 and cc >= 36) or (bc >= 36 and cc >= 36):
+    if (ac >= 37 and bc >= 37) or (ac >= 37 and cc >= 37) or (bc >= 37 and cc >= 37):
         print("E")
 else:
     print("N")
