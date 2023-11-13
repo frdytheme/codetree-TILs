9b891c5f9a3ae2fb3aa5268e0d7c7710d4833438
frdytheme <rbfridayy@gmail.com>
2023-11-13 21:09:31 +0900

    Title: a/b 출력, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 231113/a/b 출력/a-divide-b.py
+++ 231113/a/b 출력/a-divide-b.py
@@ -1,8 +1,8 @@
 num = input().split()
 a, b = int(num[0]), int(num[1])
-c = f"{a / b}"
-
-for _ in range(19):
-    c += '0'
-
-print(c)
+print(f"{a//b}.", end="")
+a %= b
+for _ in range(20):
+    a *= 10
+    print(a // b, end="")
+    a %= b
