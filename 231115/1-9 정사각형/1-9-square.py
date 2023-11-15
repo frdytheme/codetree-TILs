a644d778fb165d6d785de1a81502e3e67fc5d9d3
frdytheme <rbfridayy@gmail.com>
2023-11-15 14:57:51 +0900

    Title: 1-9 정사각형, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231115/1-9 정사각형/1-9-square.py
@@ -0,0 +1,10 @@
+n = int(input())
+cnt = 1
+for _ in range(n):
+    for _ in range(n):
+        print(cnt ,end="")
+        if cnt == 9:
+            cnt = 1
+        else:
+            cnt += 1
+    print()
