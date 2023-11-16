df9392bcc2c3747239461533e564a2c1ed37926b
frdytheme <rbfridayy@gmail.com>
2023-11-16 13:41:28 +0900

    Title: 연속부분수열일까, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

--- 231116/연속부분수열일까/contiguous-array-or-not.py
+++ 231116/연속부분수열일까/contiguous-array-or-not.py
@@ -4,7 +4,7 @@ B = list(map(int, input().split()))
 
 # 4 4 1 4
 # 4 3
-
+done = False
 for i in range(n1):
     check = True
     for j in range(n2):
@@ -15,6 +15,9 @@ for i in range(n1):
             check = False
             break
     if check:
-        print("Yes")
+        done = True
         break
-print("No")
+if done:
+    print("Yes")
+else:
+    print("No")
