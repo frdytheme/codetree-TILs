01461a758cffbc7cf37016c78e8f031263b8f632
frdytheme <rbfridayy@gmail.com>
2023-11-16 12:53:28 +0900

    Title: 배열 놀이, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231116/배열 놀이/play-with-array.py
@@ -0,0 +1,15 @@
+n, q = tuple(map(int, input().split()))
+nums = list(map(int, input().split()))
+
+for _ in range(q):
+    qu = list(map(int, input().split()))
+    if qu[0] == 1:
+        print(nums[qu[1] - 1])
+    if qu[0] == 2:
+        if qu[1] in nums:
+            print(nums.index(qu[1]) + 1)
+        else:
+            print(0)
+    if qu[0] == 3:
+        for i in range(qu[1], qu[2] + 1):
+            print(nums[i -1],end=" ")
