fda4f0f7c0acfb935ba1f6e4d7b86159eebc3fb6
frdytheme <rbfridayy@gmail.com>
2023-11-16 11:36:59 +0900

    Title: 십의 자리 숫자, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 231116/십의 자리 숫자/number-of-tens-digit.py
+++ 231116/십의 자리 숫자/number-of-tens-digit.py
@@ -2,10 +2,10 @@ nums = list(map(int, input().split()))
 arr = [0] * 9
 
 for n in nums:
-    if n < 10:
-        continue
     if n == 0:
         break
+    if n < 10:
+        continue
     a = n // 10
     arr[a-1] += 1
 for i in range(1, 10):
