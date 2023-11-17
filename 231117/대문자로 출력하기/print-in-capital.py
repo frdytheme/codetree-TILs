cb353aca5e314ecb0f0f494610b24d6656c695c1
frdytheme <rbfridayy@gmail.com>
2023-11-17 18:07:41 +0900

    Title: 대문자로 출력하기, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231117/대문자로 출력하기/print-in-capital.py
+++ 231117/대문자로 출력하기/print-in-capital.py
@@ -3,7 +3,7 @@ s = input()
 for i in range(len(s)):
     c = s[i]
     if ord(c) >= ord("A") and ord(c) <= ord("z"):
-        if ord(c) >= ord("Z"):
+        if ord(c) >= ord("a"):
             # idx = ord(c) + ord("A") - ord("a")
             # print(chr(idx), end="")
             print(c.upper(), end="")
