72e8e39a38a6f01100d9c9c032f9767ade3cad34
frdytheme <rbfridayy@gmail.com>
2023-11-17 18:59:08 +0900

    Title: 미는 횟수, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 231117/미는 횟수/number-of-pushes.py
+++ 231117/미는 횟수/number-of-pushes.py
@@ -1,9 +1,15 @@
 A, B = input(), input()
 
 cnt = 0
-
-while A != B:
+check = False
+for i in range(len(A) - 1):
     A = A[-1] + A[:-1]
     cnt += 1
-    
-print(cnt)
+    if A == B:
+        check = True
+        break
+        
+if check:
+    print(cnt)
+else:
+    print(-1)
